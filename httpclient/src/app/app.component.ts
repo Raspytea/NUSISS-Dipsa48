235223a7f1a1190dca1a17aec772f63b3f07042c
Roy Oon <roy_oon00@hotmail.com>
2019-08-23 12:27:13 +0800

    dropdown and img change

--- httpclient/src/app/app.component.ts
+++ httpclient/src/app/app.component.ts
@@ -15,6 +15,31 @@ export class AppComponent implements OnInit{
 
   }
 
+  countries = [
+    {city: "Singapore"},
+    {city: "London"},
+    {city: "Bangkok"}
+  ]
+
+  changeCity($event){
+    console.log($event.target.value)
+    this.weatherSvc.getWeather($event.target.value, this.WEATHER_API_KEY).then((result)=>{
+      this.model = new Weather(result.main.temp,result.main.pressure,result.main.humidity,result.weather[0].description,result.wind.deg,result.wind.speed);
+      if ($event.target.value == "Bangkok") {
+        document.getElementsByTagName("img")[0].src = "http://www.orangesmile.com/common/img_city_maps/bangkok-map-1.jpg";  
+      }
+      else if ($event.target.value == "London") {
+        document.getElementsByTagName("img")[0].src = "https://images-na.ssl-images-amazon.com/images/I/A17jCmQBvqL._SL1500_.jpg";  
+      }
+      else{
+        document.getElementsByTagName("img")[0].src = "https://www.nea.gov.sg/assets/images/map/base-853.png";  
+      }
+      
+    }).catch((error)=>{
+      console.log(error);
+    })
+  }
+
   ngOnInit(){
     console.log("retrieve weather !")
     this.weatherSvc.getWeather("Singapore", this.WEATHER_API_KEY).then((result)=>{
